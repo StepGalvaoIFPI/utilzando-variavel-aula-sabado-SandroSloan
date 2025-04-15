d2647641d4109d067788a4fd096ef83d02073be2
SandroSloan <111158884+SandroSloan@users.noreply.github.com>
2025-04-15 20:31:37 -0300

    Update q1.py
    eu fiz toda a atividade em um código só

--- q1.py
+++ q1.py
@@ -1,1 +1,71 @@
+#questao 1
+nome = "Sandro Sloan"
+print (nome)
+#questao 2
+nome_meu = "Sandro Sloan"
+meu_curso = "Licenciatura em computação"
+print (nome_meu)
+print (meu_curso)
+#questao 3
+nome_meu = "Sandro Sloan"
+minha_idade = 17
+print (nome_meu)
+print (minha_idade)
+#questao 4
+a = 3
+b = 5
+resultado = 2*a * 3*b
+print (resultado)
+#questao 5
+num1 = 10
+num2 = 5
+num3 = 8
+media = ((num1 + num2 + num3)/3)
+print (media)
+#questao 6
+salario = 1050
+imposto = (0.2*salario)
+print (imposto)
+#questao 7
+altura = 8
+base = 10
+multiplicacao = (altura * base)
+area = (multiplicacao / 2)
+print (area)
+#questao 8
+nota1 = 8
+nota2 = 5
+nota3 = 7
+media = ((nota1 + nota2 + nota3)/3)
+print (media)
+#questao 9
+salario_bruto = 1500
+imposto = (0.15 * salario_bruto)
+salario_liquido = (salario_bruto - imposto)
+print(salario_liquido)
+#questao 10
+salario_bruto = 5000
+valor_inss = (0.15 * salario_bruto)
+valor_ir = (0.1 * salario_bruto)
+salario_liquido = (salario_bruto - (valor_inss + valor_ir))
+print (salario_bruto)
+print (valor_inss)
+print (valor_ir)
+print (salario_liquido)
+#questao 11
+pi = 3.1415
+raio = 10
+area = (pi *(raio**2))
+print (area)
+#questao 12
+a = 8.8
+b = 7.8
+media = (((a*3.5) + (b*7.5)) / 11)
+print (media)
+
+
+
+
+
+
 
